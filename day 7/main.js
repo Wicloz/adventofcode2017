0eb86e05c783d7bfe748bf9206896f51303b0fbe
Wilco de Boer <wfh@wilcodeboer.me>
2017-12-08 22:46:02 +0100

    Day 7, part 2

--- day 7/main.js
+++ day 7/main.js
@@ -5,7 +5,7 @@ console.log(input);
 class Program {
   constructor(name, weight, holding) {
     this._name = name;
-    this._weight = weight;
+    this._weight = parseInt(weight);
     this._holdingSimple = holding;
     this._holding = [];
   }
@@ -25,6 +25,65 @@ class Program {
     }
 
     return added;
+  }
+
+  getTotalWeight() {
+    let total = this._weight;
+    this._holding.forEach((parent) => {
+      total += parent.getTotalWeight();
+    });
+    return total;
+  }
+
+  getBalancedWeight() {
+    let weights = {};
+    let maxCount = 0;
+    let maxWeight = 0;
+
+    this._holding.forEach((parent) => {
+      let weight = parent.getTotalWeight();
+
+      if (weights[weight] == null) {
+        weights[weight] = 1;
+      } else {
+        weights[weight]++;
+      }
+
+      if (weights[weight] > maxCount) {
+        maxCount = weights[weight];
+        maxWeight = weight;
+      }
+    });
+
+    return maxWeight;
+  }
+
+  hasUnbalancedGrandparents() {
+    for (let parent of this._holding) {
+      let firstWeight = -1;
+      for (let grandParent of parent._holding) {
+        if (firstWeight === -1) {
+          firstWeight = grandParent.getTotalWeight();
+        } else if (firstWeight !== grandParent.getTotalWeight()) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  getUnbalancedParent() {
+    let balance = this.getBalancedWeight();
+
+    for (let parent of this._holding) {
+      if (parent.getTotalWeight() !== balance) {
+        return parent;
+      }
+    }
+  }
+
+  getNewWeigth(balance) {
+    return this._weight + balance - this.getTotalWeight();
   }
 }
 
@@ -54,3 +113,9 @@ for (let i = 0; i < programs.length; i++) {
 
 let root = programs[0];
 console.log(root);
+
+let temp = root;
+while (temp.hasUnbalancedGrandparents()) {
+  temp = temp.getUnbalancedParent();
+}
+console.log(temp.getUnbalancedParent().getNewWeigth(temp.getBalancedWeight()));
